78ef17928f0fb0c78076ef582f10a64d98bf86f1
Shaquille Pearson <shaquillepearson9@gmail.com>
2025-06-09 13:46:52 -0400

    added fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center h-screen">
+    <main className="flex justify-center items-center h-screen flex-col gap-1">
       <h1 className="text-3xl font-bold">Hello World</h1>
     </main>
       );
